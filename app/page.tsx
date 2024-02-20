27dcd532c31fc6ad18fa38458f8f8cb7104fcb9a
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2024-02-20 15:40:13 +0600

    navbar & dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Button variant="destructive">Click me</Button>
-    </div>
-  );
+  return <div>Home</div>;
 }
