1444530a43505c26521e1e958a26d55d88a1cd42
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2024-05-26 18:49:29 +0600

    get hotels

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { getHotels } from "@/actions/getHotels";
+import HotelList from "@/components/hotel/HotelList";
 
-export default function Home() {
-  return <div>Home</div>;
+interface props {
+  searchParams: {
+    title: string;
+    country: string;
+    state: string;
+    city: string;
+  };
 }
+
+const HomePage = async ({ searchParams }: props) => {
+  const hotels = await getHotels(searchParams);
+
+  if (!hotels) {
+    return <div>No hotel found...</div>;
+  }
+  return <HotelList hotels={hotels} />;
+};
+
+export default HomePage;
