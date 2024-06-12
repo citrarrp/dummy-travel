7695f1695828bfbda64e007a6f4e3e0b870c3fc8
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2024-06-12 14:02:30 +0600

    room booking store

--- app/hotel-details/[hotelId]/page.tsx
+++ app/hotel-details/[hotelId]/page.tsx
@@ -1,5 +1,24 @@
-const hotelDetailsPage = () => {
-  return <div>hotelDetailsPage</div>;
+import { getHotelById } from "@/actions/getHotelById";
+import HotelDetailsClient from "@/components/hotel/HotelDetailsClient";
+import { notFound } from "next/navigation";
+
+interface HotelDetailsPageProps {
+  params: {
+    hotelId: string;
+  };
+}
+
+const hotelDetailsPage = async ({ params }: HotelDetailsPageProps) => {
+  const hotel = await getHotelById(params.hotelId);
+
+  if (!hotel) {
+    return notFound();
+  }
+  return (
+    <div>
+      <HotelDetailsClient hotel={hotel} />
+    </div>
+  );
 };
 
 export default hotelDetailsPage;
